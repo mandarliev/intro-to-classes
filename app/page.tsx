65d99bd945137580157a1cb2223280d8d8db9f76
Dimitar <dimitar.mandarliev@gmail.com>
2023-12-27 17:41:30 +0200

    intro-to-classes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex justify-between">
+    <div className="flex min-h-screen w-full flex-col justify-between">
       <div className="h-10 flex-1 bg-red-500"></div>
       <div className="h-10 flex-1 bg-green-500"></div>
       <div className="h-10 flex-1 bg-blue-500"></div>
